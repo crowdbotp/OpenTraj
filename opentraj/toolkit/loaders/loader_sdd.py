32dfeda188ea78004a1bb53ec4e2a41eb3ccf967
Jean Bernard Hayet <jbhayet@cimat.mx>
2022-12-21 16:53:40 -0600

    Downsampling original SDD

--- opentraj/toolkit/loaders/loader_sdd.py
+++ opentraj/toolkit/loaders/loader_sdd.py
@@ -7,72 +7,67 @@ from opentraj.toolkit.core.trajdataset import TrajDataset
 sys.path.append('.')
 
 def load_sdd(path, **kwargs):
-    sdd_dataset = TrajDataset()
-    sdd_dataset.title = kwargs.get("title", "SDD")
+	sdd_dataset = TrajDataset()
+	sdd_dataset.title = kwargs.get("title", "SDD")
 
-    csv_columns = ["agent_id", "x_min", "y_min", "x_max", "y_max", "frame_id",
-                   "lost", "occluded", "generated", "label"]
-    scale = kwargs.get("scale", 1)
+	csv_columns = ["agent_id", "x_min", "y_min", "x_max", "y_max", "frame_id",
+				   "lost", "occluded", "generated", "label"]
+	scale = kwargs.get("scale", 1)
+	# read from csv => fill traj table
+	raw_dataset = pd.read_csv(path, sep=" ", header=None, names=csv_columns)
+	raw_dataset["pos_x"] = scale * (raw_dataset["x_min"] + raw_dataset["x_max"]) / 2
+	raw_dataset["pos_y"] = scale * (raw_dataset["y_min"] + raw_dataset["y_max"]) / 2
 
-    # read from csv => fill traj table
-    raw_dataset = pd.read_csv(path, sep=" ", header=None, names=csv_columns)
-    raw_dataset["pos_x"] = scale * (raw_dataset["x_min"] + raw_dataset["x_max"]) / 2
-    raw_dataset["pos_y"] = scale * (raw_dataset["y_min"] + raw_dataset["y_max"]) / 2
+	drop_lost_frames = kwargs.get('drop_lost_frames', False)
+	filter_label = kwargs.get('label', 'Pedestrian')
+	if drop_lost_frames:
+		raw_dataset = raw_dataset.loc[raw_dataset["lost"] != 1]
+	raw_dataset = raw_dataset.loc[raw_dataset['label'] == filter_label]
 
-    drop_lost_frames = kwargs.get('drop_lost_frames', False)
-    filter_label = kwargs.get('label', 'Pedestrian')
-    if drop_lost_frames:
-        raw_dataset = raw_dataset.loc[raw_dataset["lost"] != 1]
-    raw_dataset = raw_dataset.loc[raw_dataset['label'] == filter_label]
+	# copy columns
+	sdd_dataset.data[["frame_id", "agent_id",
+					  "pos_x", "pos_y",
+					  # "x_min", "y_min", "x_max", "y_max",
+					  "label", "lost", "occluded", "generated"]] = \
+		raw_dataset[["frame_id", "agent_id",
+					 "pos_x", "pos_y",
+					 # "x_min", "y_min", "x_max", "y_max",
+					 "label", "lost", "occluded", "generated"]]
+	sdd_dataset.data["scene_id"] = kwargs.get("scene_id", 0)
 
-    # copy columns
-    sdd_dataset.data[["frame_id", "agent_id",
-                      "pos_x", "pos_y",
-                      # "x_min", "y_min", "x_max", "y_max",
-                      "label", "lost", "occluded", "generated"]] = \
-        raw_dataset[["frame_id", "agent_id",
-                     "pos_x", "pos_y",
-                     # "x_min", "y_min", "x_max", "y_max",
-                     "label", "lost", "occluded", "generated"]]
-    sdd_dataset.data["scene_id"] = kwargs.get("scene_id", 0)
-
-    # calculate velocities + perform some checks
-    fps = 30
-    sampling_rate = kwargs.get('sampling_rate', 1)
-    use_kalman = kwargs.get('use_kalman', False)
-    sdd_dataset.postprocess(fps=fps, sampling_rate=sampling_rate, use_kalman=use_kalman)
-
-    return sdd_dataset
+	# calculate velocities + perform some checks
+	fps = 30
+	sampling_rate = kwargs.get('sampling_rate', 1)
+	use_kalman = kwargs.get('use_kalman', False)
+	sdd_dataset.postprocess(fps=fps, sampling_rate=sampling_rate, use_kalman=use_kalman)
+	return sdd_dataset
 
 
 def load_sdd_dir(path: str, **kwargs):
-    search_filter_str = "**/annotations.txt"
-    if not path.endswith("/"):
-        search_filter_str = "/" + search_filter_str
-    files_list = sorted(glob.glob(path + search_filter_str, recursive=True))
-    scales_yaml_file = os.path.join(path, 'estimated_scales.yaml')
-    with open(scales_yaml_file, 'r') as f:
-        scales_yaml_content = yaml.load(f, Loader=yaml.FullLoader)
+	search_filter_str = "**/annotations.txt"
+	if not path.endswith("/"):
+		search_filter_str = "/" + search_filter_str
+	files_list = sorted(glob.glob(path + search_filter_str, recursive=True))
+	scales_yaml_file = os.path.join(path, 'estimated_scales.yaml')
+	with open(scales_yaml_file, 'r') as f:
+		scales_yaml_content = yaml.load(f, Loader=yaml.FullLoader)
 
-    partial_datasets = []
-    for file in files_list:
-        dir_names = file.split('/')
-        scene_name = dir_names[-3]
-        scene_video_id = dir_names[-2]
-        scale = scales_yaml_content[scene_name][scene_video_id]['scale']
+	partial_datasets = []
+	for file in files_list:
+		dir_names = file.split('/')
+		scene_name = dir_names[-3]
+		scene_video_id = dir_names[-2]
+		scale = scales_yaml_content[scene_name][scene_video_id]['scale']
 
-        partial_dataset = load_sdd(file, scale=scale,
-                                   scene_id=scene_name+scene_video_id.replace('video', ''))
-        partial_datasets.append(partial_dataset.data)
+		partial_dataset = load_sdd(file, scale=scale,
+								   scene_id=scene_name+scene_video_id.replace('video', ''))
+		partial_datasets.append(partial_dataset.data)
 
-    traj_dataset = TrajDataset()
-    traj_dataset.data = pd.concat(partial_datasets)
+	traj_dataset = TrajDataset()
+	traj_dataset.data = pd.concat(partial_datasets)
 
-    fps = 30
-    sampling_rate = kwargs.get('sampling_rate', 1)
-    use_kalman = kwargs.get('use_kalman', False)    
-    traj_dataset.postprocess(fps=fps, sampling_rate=sampling_rate, use_kalman=use_kalman)
-    return traj_dataset
-
-
-
+	fps = 30
+	sampling_rate = kwargs.get('sampling_rate', 1)
+	use_kalman = kwargs.get('use_kalman', False)
+	traj_dataset.postprocess(fps=fps, sampling_rate=sampling_rate, use_kalman=use_kalman)
+	return traj_dataset
--- sdd_load_example.py
+++ sdd_load_example.py
@@ -6,36 +6,41 @@ from opentraj.toolkit.loaders.loader_sdd import load_sdd, load_sdd_dir
 sys.path.append('.')
 
 scenes = {
-    'bookstore' : 7,
-    'coupa' : 4,
-    'deathCircle' : 5,
-    'gates' : 9,
-    'hyang' : 15,
-    'little' : 4,
-    'nexus' : 12,
-    'quad' : 4
+	'bookstore' : 7,
+	'coupa' : 4,
+	'deathCircle' : 5,
+	'gates' : 9,
+	'hyang' : 15,
+	'little' : 4,
+	'nexus' : 12,
+	'quad' : 4
 }
 # Fixme: set proper OpenTraj directory
-sdd_root = os.path.join('/Users/marcemq/repos/OpenTraj', 'datasets', 'SDD')
+sdd_root = os.path.join('/home/jbhayet/opt/repositories/devel/trajpred-bdl/datasets/', 'sdd_raw/annotations')
 
-sdd_data = {key: pd.DataFrame() for key in scenes.keys() } 
+sdd_data = {key: pd.DataFrame() for key in scenes.keys() }
 
 for scene_name, total_videos_per_scene in scenes.items():
-    scene_video_ids = ['video'+str(i) for i in range(total_videos_per_scene)]
-    traj_datasets_per_scene = []
+	print("Scene {} ".format(scene_name))
+	scene_video_ids = ['video'+str(i) for i in range(total_videos_per_scene)]
+	traj_datasets_per_scene = []
 
-    for scene_video_id in scene_video_ids:
-        annot_file = os.path.join(sdd_root, scene_name, scene_video_id, 'annotations.txt')
+	for scene_video_id in scene_video_ids:
+		annot_file = os.path.join(sdd_root, scene_name, scene_video_id, 'annotations.txt')
+		print(annot_file)
+		# load the homography values
+		with open(os.path.join(sdd_root, 'estimated_scales.yaml'), 'r') as hf:
+			scales_yaml_content = yaml.load(hf, Loader=yaml.FullLoader)
+		scale = scales_yaml_content[scene_name][scene_video_id]['scale']
 
-        # load the homography values
-        with open(os.path.join(sdd_root, 'estimated_scales.yaml'), 'r') as hf:
-            scales_yaml_content = yaml.load(hf, Loader=yaml.FullLoader)
-        scale = scales_yaml_content[scene_name][scene_video_id]['scale']
-
-        itraj_dataset = load_sdd(annot_file, scale=scale, scene_id=scene_name + '-' + scene_video_id,
-                        drop_lost_frames=False, use_kalman=False, label='Pedestrian')
-        traj_datasets_per_scene.append(pd.concat([itraj_dataset.data.iloc[:, : 4], itraj_dataset.data.iloc[:, 8:9]], axis=1))
-
-    pickle_out = open(scene_name+'.pickle',"wb")
-    pickle.dump(pd.concat(traj_datasets_per_scene), pickle_out, protocol=2)
-    pickle_out.close()
+		itraj_dataset = load_sdd(annot_file, scale=scale, scene_id=scene_name + '-' + scene_video_id,
+						drop_lost_frames=False, use_kalman=False, label='Pedestrian',sampling_rate=12)
+		test = itraj_dataset.data.to_numpy()[:,0]
+		#print(itraj_dataset.data["frame_id"])
+		#print(itraj_dataset.data["agent_id"])
+		traj_datasets_per_scene.append(pd.concat([itraj_dataset.data.iloc[:, : 4], itraj_dataset.data.iloc[:, 8:9]], axis=1))
+	traj_datasets_per_scene = pd.concat(traj_datasets_per_scene)
+	test = traj_datasets_per_scene.to_numpy()[:,0]
+	pickle_out = open(scene_name+'.pickle',"wb")
+	pickle.dump(traj_datasets_per_scene, pickle_out, protocol=2)
+	pickle_out.close()
